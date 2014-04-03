14d3e5ad23e2e3b3746b96c2bd129ea3c1edc25d
jezjestem <jez@obin.org>
2014-04-03 14:49:53 +0000

    fixed problem with blank bars in a chart

--- munin/DHT11.py
+++ munin/DHT11.py
@@ -2,6 +2,7 @@
 # -*- coding:utf-8 -*-
 
 import os, sys, string, glob, socket, json
+import syslog
 import dhtreader
 
 plugin_name = list(os.path.split(sys.argv[0]))[1]
@@ -15,7 +16,14 @@ if arg == "config":
 	print("h.draw AREA");
 else:
 	dhtreader.init()
-	t, h = dhtreader.read(11, 25)
+	read_loop = True
+	while read_loop:
+		try:
+			t, h = dhtreader.read(11, 25)
+		except Exception as e:
+			syslog.syslog(syslog.LOG_ERR, "read exception: %s" %(e));
+		else:
+			read_loop = False
 	print("h.value {0}".format(h))
 	
 	
